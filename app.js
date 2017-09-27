cab49914f2f62b8fc1bc837b065013e29b423b24
karlaNava <karla.nm8@gmail.com>
2017-09-26 20:57:05 -0500

    Corregí comentarios

--- app.js
+++ app.js
@@ -5,35 +5,34 @@ var spaces = document.getElementsByClassName('space');
 var bombs = document.getElementsByClassName('bomb');
 var numbers = document.getElementsByClassName('number');
 
-// loop - empty spaces to color
+// loop - empty spaces to color space
 for (var i = 0 ; i < spaces.length; i++) {
   spaces[i].addEventListener('click', changeColor, false);
 }
 
-// function to change the color of empty spaces
+// function - change empty spaces to color
 function changeColor() {
 	this.style.backgroundColor = '#000000';
 }
 
-// loop - empty spaces to color
+// loop - add numbers
 for (var i = 0 ; i < numbers.length; i++) {
   numbers[i].addEventListener('click', showNumber, false);
 }
 
-// function to show the number of bombs near
+// function - show the number of bombs near
 function showNumber() {
 	this.innerText = "1";
 }
 
-// loop - empty spaces to color
+// loop - to add bombs
 for (var i = 0 ; i < bombs.length; i++) {
   bombs[i].addEventListener('click', BombIt, false);
 }
 
-// function to the number of bombs near
+// function - explode bomb
 function BombIt() {
 	alert(' YOU LOST :( sorry! ');
-
   containerBombIt.removeChild(tableBombIt);
   containerBombIt.innerHTML = "<img src='https://media.giphy.com/media/3osxYCsLd9qgsgqpwI/giphy.gif' id='imgBomb' alt='BOOM!'>"
 }
@@ -42,6 +41,7 @@ function BombIt() {
 var buttonReStart = document.getElementById('reStart');
 buttonReStart.addEventListener('click', reStart);
 
+// function - restart the game
 function reStart() {
   var imgBomb = document.getElementById('imgBomb');
   containerBombIt.removeChild(imgBomb);
